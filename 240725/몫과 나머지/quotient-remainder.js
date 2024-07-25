b7a091850e41502a8e5d5a0b1e619ff42abb4c6e
woals7480 <94383932+woals7480@users.noreply.github.com>
2024-07-25 17:25:06 +0900

    Title: 몫과 나머지, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240725/몫과 나머지/quotient-remainder.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split(' ');
+const a = Number(input[0]);
+const b = Number(input[1]);
+console.log(`${parseInt(a/b)}...${a%b}`)
